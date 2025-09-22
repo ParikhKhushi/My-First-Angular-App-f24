e984f87622ebbb0e0cc68d2a2d3ce6d75fe6e787
ParikhKhushi <w0827081@myscc.ca>
2025-09-21 22:34:19 -0400

    Lecture 2 class exercise

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from '../shared/models/User';
+import {JsonPipe,NgForOf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('My-First-Project');
-}
+export class AppComponent {
+  title= 'User Generation';
+
+  user1 : User = {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false};
+  user2 : User = {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true};
+  //Can declare values either way
+  userList: User[] = //any[] would have worked as well
+    [this.user1,this.user2,
+      {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
+      {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+    ]
+//Function that gets called from our onclick. Takes in an
+  //arguement ofa variable called user, which is type User and returns void
+  toggleAdminStatus(user: User): void {
+    user.isAdmin = !user.isAdmin;
+  }}
